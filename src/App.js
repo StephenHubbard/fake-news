265581a5f86bef349ea3d83310b7bfd30f08b1d3
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-01-28 16:35:30 -0700

    bootstrap template in place and basic back end set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './Components/Header';
 import Routes from './Routes';
+import Footer from './Components/Footer';
+
 
 
 
@@ -8,6 +10,7 @@ const App = () => (
   <div className="App">
     <Header />
     {Routes}
+    <Footer />
   </div>
 );
 
