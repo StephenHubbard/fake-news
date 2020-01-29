176acd53e7c94fc7dcd37b25eeb16e4798c42ab0
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-01-29 08:20:19 -0700

    added footer and router set up

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,14 +12,17 @@ function Header(res) {
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="#home">Hooks-News</Navbar.Brand>
                 <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Post</Nav.Link>
-                <Nav.Link href="#pricing">About</Nav.Link>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#post">Post</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
                 </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-                </Form>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-info">Search</Button>
+                    </Form>
+                <Nav className="d-flex justify-content-end">
+                    <Nav.Link href="#login">Login</Nav.Link>
+                </Nav>
             </Navbar>
 		</div>
 	);
